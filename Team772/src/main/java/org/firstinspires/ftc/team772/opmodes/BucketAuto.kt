09355b4fc3dfa08f2b2aea114218159306d9369f
Alberto <albertmathisz0@gmail.com>
2025-02-21 13:00:41 -0500

    Bucket Auto being worked on

--- Team772/src/main/java/org/firstinspires/ftc/team772/opmodes/BucketAuto.kt
+++ Team772/src/main/java/org/firstinspires/ftc/team772/opmodes/BucketAuto.kt
@@ -5,11 +5,14 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry
 import com.arcrobotics.ftclib.command.CommandOpMode
 import com.arcrobotics.ftclib.command.RunCommand
 import com.arcrobotics.ftclib.command.SequentialCommandGroup
+import com.arcrobotics.ftclib.command.WaitCommand
 import com.arcrobotics.ftclib.command.WaitUntilCommand
+import com.pedropathing.commands.FollowPath
 import com.pedropathing.follower.Follower
 import com.pedropathing.localization.Pose
 import com.pedropathing.util.Constants
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import org.firstinspires.ftc.team772.auto.BucketAutoPaths
 import org.firstinspires.ftc.team772.implementation.ClimbSystem
 import org.firstinspires.ftc.team772.implementation.IntakeSystem
 import org.firstinspires.ftc.team772.implementation.OuttakeSystem
@@ -38,7 +41,22 @@ class BucketAuto(): CommandOpMode() {
             }),
             SequentialCommandGroup(
                 // preload
-                outtakeSystem.toggleClaw()
+                FollowPath(follower, BucketAutoPaths.scorePreloadPath, true, 0.2),
+                climbSystem.setTargetPosition(ClimbSystem.ClimbState.HIGH_BASKET),
+                outtakeSystem.moveArmToScore(),
+                WaitCommand(1000),
+                outtakeSystem.toggleClaw(),
+                outtakeSystem.moveArmToHome(),
+                climbSystem.setTargetPosition(ClimbSystem.ClimbState.HOME),
+                //Field sample 1
+                FollowPath(follower, BucketAutoPaths.moveToFirstSample, true, 0.2),
+                intakeSystem.moveToTarget()
+
+
+
+
+
+
                 //
 
             )
