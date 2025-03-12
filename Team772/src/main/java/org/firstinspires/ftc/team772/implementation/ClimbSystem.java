a471204ba1daacaeceeb7e5761dd6dafaa2e5c76
Alberto <albertmathisz0@gmail.com>
2025-03-12 08:38:00 -0400

    Re-Tuned slide PID

--- Team772/src/main/java/org/firstinspires/ftc/team772/implementation/ClimbSystem.java
+++ Team772/src/main/java/org/firstinspires/ftc/team772/implementation/ClimbSystem.java
@@ -34,7 +34,7 @@ public class ClimbSystem extends SubsystemBase {
         }
     }
 
-    public static PIDFCoefficients PID_SLIDES = new PIDFCoefficients(0.02, 0.001, 0.0003, 0.033);
+    public static PIDFCoefficients PID_SLIDES = new PIDFCoefficients(0.007, 0.00, 0.0001, 0.05);
 
     private final DcMotorEx climbMotor1;
     private final DcMotorEx climbMotor2;
