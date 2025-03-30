4b4a659b8c8e955bf2c21809645f9487b7c846fb
Gabriel Howe <me@gabe.how>
2025-03-30 00:16:58 -0400

    set nominal voltage to sanjith specification

--- Team772/src/main/java/org/firstinspires/ftc/teamcode/implementation/Constants.java
+++ Team772/src/main/java/org/firstinspires/ftc/teamcode/implementation/Constants.java
@@ -4,7 +4,7 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class Constants {
 
-         public static double NOMINAL_BATTERY_VOLTAGE = 14.0;
+         public static double NOMINAL_BATTERY_VOLTAGE = 13.5;
 
 
         /**
